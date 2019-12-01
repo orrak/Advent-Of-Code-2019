1cafc2746ab6ee09522622c15acd9f8f46f7e182
Karro <karoliod@student.matnat.uio.no>
2019-12-01 08:32:17 +0100

    both parts

--- day1.py
+++ day1.py
@@ -1,11 +1,16 @@
-
 import numpy as np
 
-s = 0
+# Advent of Code - Day 1
+
+s1 = 0  # Sum for part 1
+s2 = 0  # Sum for part 2
+
 with open('input1.txt', 'r') as f:
     for m in f:
         m = np.floor(int(m)/3 - 2)
+        s1 += m
         while (int(m) > 0):
-            s += m
+            s2 += m
             m = np.floor(int(m)/3 - 2)
-print(s)
+print(s1)
+print(s2)
