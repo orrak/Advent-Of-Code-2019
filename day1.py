02fb888ea188981ed198e976621a2e317c36261e
Karro <karoliod@student.matnat.uio.no>
2019-12-07 08:58:31 +0100

    small changes

--- day1.py
+++ day1.py
@@ -7,10 +7,22 @@ s2 = 0  # Sum for part 2
 
 with open('input1.txt', 'r') as f:
     for m in f:
-        m = np.floor(int(m)/3 - 2)
+        m = np.floor(int(m)/3) - 2
         s1 += m
         while (int(m) > 0):
             s2 += m
-            m = np.floor(int(m)/3 - 2)
+            m = np.floor(int(m)/3) - 2
 print(s1)
 print(s2)
+
+# Part 1 alt
+
+mass = []
+with open('input1.txt', 'r') as f:
+    for m in f:
+        mass.append(int(m))
+
+mass = np.array(mass)
+fuel = np.floor(mass/3) - 2
+
+print(np.sum(fuel))
--- day4.py
+++ day4.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# range from input
+# puzzle input
 start = 264793
 stop = 803935
 
