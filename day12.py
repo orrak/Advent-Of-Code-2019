8d43b49e17f0b18b4ef27a79f2f6d52ee8ab31be
Karro <karoliod@student.matnat.uio.no>
2020-12-07 02:50:05 +0100

    day12 both parts

--- day12.py
+++ day12.py
@@ -1,63 +1,82 @@
 import re
 import numpy as np
+import math
+
+def calcvel(pos, vel):
+    for i in range(len(pos)):
+        for j in range(i, len(pos)):
+            if i != j:
+                arr1 = 1*(pos[i] > pos[j])
+                arr2 = -1*(pos[i] < pos[j])
+                arr = arr1 + arr2
+                vel[i] -= arr
+                vel[j] += arr
+    return pos, vel
+
+def checkdone(prevpos, pos, prevvel, vel, done):
+    posmatch = False
+    velmatch = False
+    for elem in prevpos:
+        if np.all(elem == pos):
+            posmatch = True
+    for elem in prevvel:
+        if np.all(elem == vel):
+            velmatch = True
+
+    if posmatch and velmatch:
+        done = True
+        print("t=%d" % t)
+
+    return prevpos, pos, prevvel, vel, done
+
+def calcenergy(pos, vel):
+    t = 0
+    while(t < 1000):
+        pos, vel = calcvel(pos, vel)
+        for i in range(len(pos)):
+            pos[i] += vel[i]
+
+        t += 1
+    energy = 0
+    for i in range(len(pos)):
+        energy += (sum(abs(pos[i])) * sum(abs(vel[i])))
+    return energy
+
+def calccycle(pos, vel):
+    done = False
+    startpos = pos.copy()
+    startvel = vel.copy()
+    t = 0
+    while(not done):
+        pos, vel = calcvel(pos, vel)
+        for i in range(len(pos)):
+            pos[i] += vel[i]
+
+        t += 1
+        if np.all(startpos == pos) and np.all(startvel == vel):
+            done = True
+            return t
 
 with open("input12.txt", "r") as f:
     lst = f.read().strip().split("\n")
     pos = np.array([[int(match) for match in re.findall(r'-?[0-9]+', elem)] for elem in lst], dtype=float)
-    # print(pos)
-
     vel = np.zeros((4,3))
-    # print(vel)
 
-    t = 0
-    while(t < 10):
-        # compare position to calc gravity
-        for i in range(len(pos)):
-            for j in range(i, len(pos)):
-                if i != j:
-                    if np.any(pos[i] == pos[j]):
-                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                    print("pos, i=%d, j=%d" % (i, j))
-                    print(pos[i])
-                    print(pos[j])
-                    print("\n")
-                    arr1 = 1*(pos[i] > pos[j])
-                    arr2 = -1*(pos[i] < pos[j])
-                    arr = arr1 + arr2
-                    print("arr1")
-                    print(arr1)
-                    print("arr2")
-                    print(arr2)
-                    print("change arr")
-                    print(arr)
-                    print("before vel, i=%d, j=%d" % (i, j))
-                    print(vel[i])
-                    print(vel[j])
-                    vel[i] -= arr
-                    vel[j] += arr
-                    print("after vel, i=%d, j=%d" % (i, j))
-                    print(vel[i])
-                    print(vel[j])
-                    print("\n")
-                    # print(1*(pos[i] == pos[j]))
-                    # print(1*(pos[i] < pos[j]))
+    energy = calcenergy(pos, vel)
 
-        for i in range(len(pos)):
-            print("pos, vel, i=%d" % i)
-            print(pos[i])
-            print(vel[i])
-            pos[i] += vel[i]
-            print("new pos, i=%d" % i)
-            print(pos[i])
-            print("\n")
+    print("part one: %d" % energy)
 
-        t += 1
 
-    energy = 0
-    for i in range(len(pos)):
-        print(pos[i], vel[i])
-        energy += sum(pos[i]) * sum(vel[i])
+with open("input12.txt", "r") as f:
+    lst = f.read().strip().split("\n")
+    pos = np.array([[int(match) for match in re.findall(r'-?[0-9]+', elem)] for elem in lst], dtype=float)
+    vel = np.zeros((4,3))
 
-    print(energy)
+    ts = []
+    for i in range(pos.shape[1]):
+        lstpos = [m[i] for m in pos]
+        lstvel = [m[i] for m in vel]
+        t = calccycle(lstpos, lstvel)
+        ts.append(t)
 
-# part one: 3910 too low, 4708 too low, 5000 too low
+    print("part two: %d" % np.lcm.reduce(ts))
